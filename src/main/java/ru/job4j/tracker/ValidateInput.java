2d6f50a54d9ad70ca84e848372d3856c07f61e96
BoykoKatty <grania@list.ru>
2023-08-02 02:58:24 +0300

    ValidateInput decorator

--- src/main/java/ru/job4j/tracker/ValidateInput.java
+++ src/main/java/ru/job4j/tracker/ValidateInput.java
@@ -2,13 +2,26 @@ package ru.job4j.tracker;
 
 public class ValidateInput extends ConsoleInput {
 
+    private final Output out;
+    private final Input in;
+
+    public ValidateInput(Output out, Input input) {
+        this.out = out;
+        this.in = input;
+    }
+
+    @Override
+    public String askStr(String question) {
+        return in.askStr(question);
+    }
+
     @Override
     public int askInt(String question) {
         int value = -1;
         try {
             value = super.askInt(question);
         } catch (NumberFormatException nfe) {
-            System.out.println("Please enter valid data");
+            out.println("Please enter valid data");
             askInt(question);
         }
         return value;

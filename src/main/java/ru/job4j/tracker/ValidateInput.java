62e5099e966c46445ae6176e24861aac0427adbc
BoykoKatty <grania@list.ru>
2023-08-02 03:56:06 +0300

    ValidateInput change

--- src/main/java/ru/job4j/tracker/ValidateInput.java
+++ src/main/java/ru/job4j/tracker/ValidateInput.java
@@ -1,16 +1,35 @@
 package ru.job4j.tracker;
 
-public class ValidateInput extends ConsoleInput {
+public class ValidateInput implements Input {
+
+    private final Output out;
+    private final Input in;
+
+    public ValidateInput(Output out, Input input) {
+        this.out = out;
+        this.in = input;
+    }
+
+    @Override
+    public String askStr(String question) {
+        return in.askStr(question);
+    }
 
     @Override
     public int askInt(String question) {
         int value;
+        String answer = askStr(question);
         try {
-            value = super.askInt(question);
+            value = Integer.parseInt(answer);
         } catch (NumberFormatException nfe) {
-            System.out.println("Please enter valid data");
+            out.println("Please enter valid data");
             value = askInt(question);
         }
         return value;
+    }
+
+    @Override
+    public void closeResource() {
+        in.closeResource();
     }
 }

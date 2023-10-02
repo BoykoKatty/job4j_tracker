411f805be8d4e39c68cffd28084eaf24a73e6513
BoykoKatty <grania@list.ru>
2023-10-02 22:40:03 +0300

    ReconstructPhrase

---
+++ src/main/java/ru/job4j/queue/ReconstructPhrase.java
@@ -0,0 +1,39 @@
+package ru.job4j.queue;
+
+import java.util.Deque;
+import java.util.Iterator;
+
+public class ReconstructPhrase {
+
+    private final Deque<Character> descendingElements;
+
+    private final Deque<Character> evenElements;
+
+    public ReconstructPhrase(Deque<Character> descendingElements, Deque<Character> evenElements) {
+        this.descendingElements = descendingElements;
+        this.evenElements = evenElements;
+    }
+
+    private String getEvenElements() {
+        StringBuilder stringBuilder = new StringBuilder();
+        boolean even = true;
+        for (Character c : evenElements) {
+            stringBuilder.append(even ? c : "");
+            even = !even;
+        }
+        return stringBuilder.toString();
+    }
+
+    private String getDescendingElements() {
+        StringBuilder stringBuilder = new StringBuilder();
+        Iterator<Character> iterator = descendingElements.descendingIterator();
+        while (iterator.hasNext()) {
+            stringBuilder.append(iterator.next());
+        }
+        return stringBuilder.toString();
+    }
+
+    public String getReconstructPhrase() {
+        return getEvenElements() + getDescendingElements();
+    }
+}

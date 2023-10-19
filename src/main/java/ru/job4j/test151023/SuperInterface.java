41b9537ea35a432a597d4f95f8206c48ccfbe064
BoykoKatty <grania@list.ru>
2023-10-19 14:33:12 +0300

    Test

---
+++ src/main/java/ru/job4j/test151023/SuperInterface.java
@@ -0,0 +1,9 @@
+package ru.job4j.test151023;
+
+public interface SuperInterface {
+    String say();
+
+    public static void sayStatic() {
+        System.out.println("yes");
+    }
+}
---
+++ src/main/java/ru/job4j/test151023/Test.java
@@ -0,0 +1,7 @@
+package ru.job4j.test151023;
+
+public class Test {
+    protected class TestInner {
+
+    }
+}

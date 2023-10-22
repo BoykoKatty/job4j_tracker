32f305800c615e92cf500a95e1728602b78a774e
BoykoKatty <grania@list.ru>
2023-10-22 12:26:16 +0300

    FunctionCalculatorTest_corrected

--- src/test/java/ru/job4j/function/FunctionCalculatorTest.java
+++ src/test/java/ru/job4j/function/FunctionCalculatorTest.java
@@ -27,8 +27,8 @@ public class FunctionCalculatorTest {
     @Test
     public void whenExponentialFunctionThenExponentialResults() {
         FunctionCalculator function = new FunctionCalculator();
-        List<Double> result = function.diapason(1, 4, x -> Math.pow(x, x));
-        List<Double> expected = Arrays.asList(1D, 4D, 27D);
+        List<Double> result = function.diapason(1, 4, x -> Math.pow(2, x));
+        List<Double> expected = Arrays.asList(2D, 4D, 8D);
         assertThat(result).containsAll(expected);
     }
 }

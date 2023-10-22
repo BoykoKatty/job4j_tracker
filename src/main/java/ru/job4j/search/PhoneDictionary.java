71cb3fde2df9114d5641f0744c1e00fda87cab53
BoykoKatty <grania@list.ru>
2023-10-22 14:17:45 +0300

    PhoneDictionary Predicate

--- src/main/java/ru/job4j/search/PhoneDictionary.java
+++ src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -1,6 +1,7 @@
 package ru.job4j.search;
 
 import java.util.ArrayList;
+import java.util.function.Predicate;
 
 public class PhoneDictionary {
     private ArrayList<Person> persons = new ArrayList<>();
@@ -16,13 +17,13 @@ public class PhoneDictionary {
      * @return Список пользователей, которые прошли проверку.
      */
     public ArrayList<Person> find(String key) {
+        Predicate<Person> combine1 = (person -> person.getName().equals(key));
+        Predicate<Person> combine2 = (person -> person.getSurname().equals(key));
+        Predicate<Person> combine3 = (person -> person.getAddress().equals(key));
+        Predicate<Person> combine4 = (person -> person.getPhone().equals(key));
         ArrayList<Person> result = new ArrayList<>();
         for (Person person : persons) {
-            if (person.getName().contains(key)
-                    || person.getSurname().contains(key)
-                    || person.getAddress().contains(key)
-                    || person.getPhone().contains(key)
-            ) {
+            if (combine1.or(combine2).or(combine3).or(combine4).test(person)) {
                 result.add(person);
             }
         }

69091f2a5c66a5391b438e8fb2dea378e9793f89
BoykoKatty <grania@list.ru>
2023-10-28 11:11:55 +0300

    ProductLabel upd

--- src/main/java/ru/job4j/stream/ProductLabel.java
+++ src/main/java/ru/job4j/stream/ProductLabel.java
@@ -5,8 +5,8 @@ import java.util.List;
 public class ProductLabel {
     public List<String> generateLabels(List<Product> products) {
         return products.stream().
-                filter(product -> product.getStandard() - product.getActual() <= 3).
-                map(product -> new Label(product.getName(), product.getPrice() / 2).toString()).
-                toList();
+                filter(product -> product.getStandard() - product.getActual() <= 3 && product.getStandard() - product.getActual() >= 0)
+                .map(product -> new Label(product.getName(), product.getPrice() / 2).toString())
+                .toList();
     }
 }
